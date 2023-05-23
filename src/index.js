fefd943dc1fcb094e2c44cda627090fa2f16f7ac
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-23 16:32:18 -0300

    linters fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css';
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hello world!";
+  element.innerHTML = 'Hello world!';
   element.classList.add('hello');
 
   return element;
