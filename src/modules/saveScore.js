ba3695d160fda1dae95ec8fc580635cd140d6cd9
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-25 00:22:22 -0300

    saveScore function expand

--- src/modules/saveScore.js
+++ src/modules/saveScore.js
@@ -1,2 +1,10 @@
-const name = document.getElementById('name-input');
-const score = document.getElementById('score-input');
+//const name = document.getElementById('name-input');
+//const score = document.getElementById('score-input');
+
+const addScore = async (name, score) => {
+  const newRecord = {
+    "user": name.value,
+    "score": score.value
+  }
+
+}
