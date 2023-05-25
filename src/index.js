49eddf3381cb06f6e4c98dc0ec1e3da1b9b4a49e
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-25 18:29:52 -0300

    addeventlistener for submit button

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import './style.css';
 import render from './modules/render.js';
 import addScore from './modules/saveScore.js';
 
+const addButton = document.getElementById('submit-button');
+
+addButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  await addScore();
+});
+
 //render();
