23d391acd897fafae0c60ebb2cf3f7fb32f3299e
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-26 14:37:56 -0300

    linters fix

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import addScore from './modules/saveScore.js';
 const addButton = document.getElementById('submit-button');
 const refresh = document.getElementById('refresh-button');
 
-
 addButton.addEventListener('click', async (event) => {
   event.preventDefault();
   await addScore();
   await render();
 });
 
-refresh.addEventListener('click', async() => {
+refresh.addEventListener('click', async () => {
   await render();
-})
+});
 
 render();
--- src/modules/saveScore.js
+++ src/modules/saveScore.js
@@ -1,15 +1,14 @@
 const nameInput = document.getElementById('name-input');
 const scoreInput = document.getElementById('score-input');
-const myAPI = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1uV0I3Da74nTfdPATfRO/scores/";
+const myAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1uV0I3Da74nTfdPATfRO/scores/';
 
 const addScore = async () => {
   const name = nameInput.value;
-  const score = Number(scoreInput.value); // Convert the score value to a number using the Number() function
+  const score = Number(scoreInput.value);
 
   // Check if the score is a valid number
   if (Number.isNaN(score)) {
-    console.log('Invalid score value. Please enter a number.');
-    return;
+    throw new Error('Invalid score value. Please enter a number.');
   }
 
   // Create the data object
@@ -20,23 +19,21 @@ const addScore = async () => {
     const response = await fetch(myAPI, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
     if (response.ok) {
       // Reset input fields
       nameInput.value = '';
       scoreInput.value = '';
-      console.log('Score added successfully.');
-    } else {
-      console.log('Failed to add score.');
+      return 'Score added successfully.';
     }
+    throw new Error('Failed to add score.');
   } catch (error) {
-    console.error('Error:', error);
+    throw new Error(`Error: ${error.message}`);
   }
 };
-
 
 export default addScore;
